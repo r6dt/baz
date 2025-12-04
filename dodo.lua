945d78446d4ecfed85e7ce802f5d11ba34f47749
r6dt <wycswy7h@gmail.com>
2025-12-04 21:44:37 +0700

    Create dodo.lua

---
+++ dodo.lua
@@ -0,0 +1,11 @@
+repeat task.wait() until game:IsLoaded()
+
+getgenv().g_config = {
+    global_workspace = true, -- (Default: true)
+    auto_hideui = true, -- (Default: false)
+    whitescreen = false, -- (Default: false)
+    headless = true -- (Default: false)
+}
+script_key="NfpFBaIfvgmDOLdlUUwkUEwulSeupYiD"
+premium=true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dodohubx-rgb/dodohub/refs/heads/main/loader.luau"))()
