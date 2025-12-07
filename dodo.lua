e4b81216face4591b42e5634f1264b4b058438f8
r6dt <wycswy7h@gmail.com>
2025-12-08 01:10:52 +0700

    Update dodo.lua

--- dodo.lua
+++ dodo.lua
@@ -1,5 +1,13 @@
 repeat task.wait() until game:IsLoaded()
 
+local args = {
+    [1] = "Disable_Weather_Mutate_Sand",
+    [2] = true
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SettingRE"):FireServer(unpack(args))
+
+task.wait(10)
 getgenv().g_config = {
     global_workspace = true, -- (Default: true)
     auto_hideui = true, -- (Default: false)
