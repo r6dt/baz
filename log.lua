a0bd6895449307487b0732876b4ee44e85d799ab
r6dt <wycswy7h@gmail.com>
2025-12-26 15:56:14 +0700

    Delete log.lua

--- log.lua
+++
@@ -1,9 +0,0 @@
-repeat task.wait() until game:IsLoaded()
-
-getgenv().DuckyConfig = {
-    ["EnableLog"] = true,
-    ["discord_id"] = "1167566243969126442",
-    ["PC_NAME"] = "ALL"
-}
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DuckyE/duckyLog/refs/heads/main/ducky.host.lua"))()
