15e70f574e79e5bae6e17029704802e0d46d7ffd
r6dt <wycswy7h@gmail.com>
2025-12-31 01:49:26 +0700

    Add dodo_ddc3.lua for weather mutation settings

---
+++ dodo_ddc3.lua
@@ -0,0 +1,18 @@
+repeat task.wait() until game:IsLoaded()
+
+local remote = game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SettingRE")
+
+remote:FireServer("Disable_Weather_Mutate_Sand", true)
+remote:FireServer("Disable_Weather_Mutate_Aurora", true)
+
+-- main
+task.wait(5)
+getgenv().g_config = {
+    global_workspace = true, -- (Default: true)
+    auto_hideui = true, -- (Default: false)
+    whitescreen = false, -- (Default: false)
+    headless = true -- (Default: false)
+}
+script_key="CyUpdWKPgIfhyZjdhWMZdtYcKyIZlnnO"
+premium=true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dodohubx-rgb/dodohub/refs/heads/main/loader.luau"))()
