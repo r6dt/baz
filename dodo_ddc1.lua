45cc564aa3e07647136d88b449d87c9fb8e987f1
r6dt <wycswy7h@gmail.com>
2026-01-23 16:20:08 +0700

    Update dodo_ddc1.lua

--- dodo_ddc1.lua
+++ dodo_ddc1.lua
@@ -1,4 +1,7 @@
 repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players
+repeat task.wait() until game.Players.LocalPlayer
+repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
 
 -- main
 task.wait(5)
