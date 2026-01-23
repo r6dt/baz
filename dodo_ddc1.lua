fa6bed53ecdda2333866e1f5efc22e22b0dd881e
r6dt <wycswy7h@gmail.com>
2026-01-23 18:20:40 +0700

    Update dodo_ddc1.lua

--- dodo_ddc1.lua
+++ dodo_ddc1.lua
@@ -3,8 +3,28 @@ repeat task.wait() until game.Players
 repeat task.wait() until game.Players.LocalPlayer
 repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
 
+-- Services
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+-- รอให้ตัวละครโหลด
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+
+-- ตำแหน่งปัจจุบัน + ทิศทางที่ตัวละครหันหน้าอยู่
+local currentCFrame = humanoidRootPart.CFrame
+
+-- ระยะที่จะ TP (30 studs)
+local distance = 100
+
+-- คำนวณตำแหน่งไปข้างหน้า
+local newCFrame = currentCFrame * CFrame.new(0, 0, -distance)
+
+-- TP
+humanoidRootPart.CFrame = newCFrame
+
 -- main
-task.wait(5)
+task.wait(10)
 getgenv().g_config = {
     global_workspace = true, -- (Default: true)
     auto_hideui = true, -- (Default: false)
@@ -14,3 +34,5 @@ getgenv().g_config = {
 script_key="CyUpdWKPgIfhyZjdhWMZdtYcKyIZlnnO"
 premium=true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dodohubx-rgb/dodohub/refs/heads/main/loader.luau"))()
+
+ใส่แบบนี้ได้ใช่มั้ย
