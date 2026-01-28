d5b7bceff9aee5e9acd252c228533e1d814aaf4e
r6dt <wycswy7h@gmail.com>
2026-01-28 14:53:13 +0700

    Update dodo_ddc2.lua

--- dodo_ddc2.lua
+++ dodo_ddc2.lua
@@ -1,18 +1,26 @@
 repeat task.wait() until game:IsLoaded()
 
-local remote = game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("SettingRE")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
 
-remote:FireServer("Disable_Weather_Mutate_Sand", true)
-remote:FireServer("Disable_Weather_Mutate_Aurora", true)
+-- รอให้ Character พร้อมจริง ๆ
+local character = player.Character or player.CharacterAdded:Wait()
+local hrp = character:WaitForChild("HumanoidRootPart")
+
+-- รอให้ Roblox เซ็ตตำแหน่งเสร็จ
+task.wait(1.5)
+
+-- TP ไปข้างหน้า 100 studs
+hrp.CFrame = hrp.CFrame * CFrame.new(0, 0, -100)
 
 -- main
-task.wait(5)
+task.wait(20)
 getgenv().g_config = {
     global_workspace = true, -- (Default: true)
     auto_hideui = true, -- (Default: false)
     whitescreen = false, -- (Default: false)
     headless = true -- (Default: false)
 }
-script_key="PMAQsHhTKHoPHRLlBtBjrzbuFjtlPKtI"
+script_key="WvPqVMkmOaTzyEIAZxiskeBfwUmaLyOV"
 premium=true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dodohubx-rgb/dodohub/refs/heads/main/loader.luau"))()
